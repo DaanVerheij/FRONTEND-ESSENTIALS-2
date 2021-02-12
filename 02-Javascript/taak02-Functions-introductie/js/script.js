b26839d7d6079f2240c81aae742af1bd4efb0f7e
Daan <daan.verheij@student.rocva.nl>
2021-02-12 12:47:10 +0100

    taak 02 functions

---
+++ 02-Javascript/taak02-Functions-introductie/js/script.js
@@ -0,0 +1,31 @@
+function toonAlertBox(){
+    alert("Alert box opgeroepen door mijn eerste functie!");
+}
+
+
+toonAlertBox();
+
+
+function alertboxPrompt(){
+    prompt("Vul hier je naam in!");
+    console.log;
+}
+
+
+alertboxPrompt();
+
+
+function confirmBox(){
+    confirm("Klik op ok of annuleren")
+    console.log;
+
+}
+
+if(confirm=true){
+    console.log("Ok!")
+}
+else(confirm=false)
+    console.log("Dan niet")
+
+
+confirmBox();
