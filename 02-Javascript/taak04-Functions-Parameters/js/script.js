e5087fe2e85e23b3e4fcea0d35112ba36c08269d
Daan <daan.verheij@student.rocva.nl>
2021-02-16 11:20:30 +0100

    taak 04 parameters

---
+++ 02-Javascript/taak04-Functions-Parameters/js/script.js
@@ -0,0 +1,8 @@
+var getal1 = prompt("Geef hier het getal 5");
+var getal2 = prompt("Geef hier het getal 4");
+
+function keerElkaar(getal1, getal2){
+    let som = getal1 * getal2;
+    return som;
+}
+console.log(keerElkaar(5,4));
