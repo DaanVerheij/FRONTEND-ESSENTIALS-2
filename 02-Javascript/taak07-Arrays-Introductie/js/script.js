7759cf79895f8f9f1581f3a72e3b6d282071d4b9
Daan <daan.verheij@student.rocva.nl>
2021-02-17 14:38:48 +0100

    taak 07 array introductie

---
+++ 02-Javascript/taak07-Arrays-Introductie/js/script.js
@@ -0,0 +1,12 @@
+topFilms = ["The Matrix", "The Hunger Games", "Taken"];
+console.log(topFilms[0]);
+console.log(topFilms[1]);
+console.log(topFilms[2]);
+topGames = ["Red Dead Redemption 2", "Gta V", "Far Cry Primal"];
+console.log(topGames[0]);
+console.log(topGames[1]);
+console.log(topGames[2]);
+topFruit = ["Banaan", "Kiwi", "Perzik"];
+console.log(topFruit[0]);
+console.log(topFruit[1]);
+console.log(topFruit[2]);
