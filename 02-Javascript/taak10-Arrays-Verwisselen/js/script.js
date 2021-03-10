8a7ba1de842c0679da8531411aa20a433440c74f
Daan <daan.verheij@student.rocva.nl>
2021-03-10 15:00:32 +0100

    taak 10 array verwisselen(eindelijk af)

--- 02-Javascript/taak10-Arrays-Verwisselen/js/script.js
+++ 02-Javascript/taak10-Arrays-Verwisselen/js/script.js
@@ -5,6 +5,13 @@ topFilms = [
     "Raging Bull",
     "Casablanca"
 ];
+var item = "";
+
+fillTable();
+
+
+
+function fillTable(){
 
 document.querySelector(".film1-ranking").innerHTML = 1;
 document.querySelector(".film1-naam").innerHTML = topFilms[0];
@@ -16,5 +23,51 @@ document.querySelector(".film4-ranking").innerHTML = 4;
 document.querySelector(".film4-naam").innerHTML = topFilms[3];
 document.querySelector(".film5-ranking").innerHTML = 5;
 document.querySelector(".film5-naam").innerHTML = topFilms[4];
+}
+
+function moveUp(cijfer, richting){
+   
+    if(cijfer == 0 && richting == "up" || cijfer == 4 && richting == "down" ){
+        alert("Kan niet!");   
+    }
+    if((cijfer == 0 && richting == "down") || (cijfer == 1 && richting == "up")){
+        item = topFilms[0];
+      
+        topFilms[0] = topFilms[1];
+        topFilms[1] = item;
+
+    }
+    if((cijfer == 1 && richting == "down") || (cijfer == 2 && richting == "up")){
+        item = topFilms[1];
+      
+        topFilms[1] = topFilms[2];
+        topFilms[2] = item;
+        }
+    if((cijfer == 2 && richting == "down") || (cijfer == 3 && richting == "up")){
+        item = topFilms[2];
+      
+        topFilms[2] = topFilms[3];
+        topFilms[3] = item;
+        }
+    if((cijfer == 3 && richting == "down") || (cijfer == 4 && richting == "up")){
+        item = topFilms[3];
+      
+        topFilms[3] = topFilms[4];
+        topFilms[4] = item;
+        }
+
+fillTable();
+    
+}
 
 
+function resetList(){
+    topFilms = [
+        "The Godfather",
+        "The Shawshank Redemption",
+        "Schindler's List",
+        "Raging Bull",
+        "Casablanca"
+    ];
+    fillTable();
+}
