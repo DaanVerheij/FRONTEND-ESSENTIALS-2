96d9b5f80e52cda839cbab4c7a209208e243ab07
Daan <daan.verheij@student.rocva.nl>
2021-03-11 11:07:51 +0100

    taak  14 loop

--- 02-Javascript/taak14-Loops-Strings/js/script.js
+++ 02-Javascript/taak14-Loops-Strings/js/script.js
@@ -5,5 +5,19 @@ function outputLetters(str){
     }
 }
 
-var voorbeeldString = "Dit is een array van letters";
-console.log(voorbeeldString.length); // geeft terug: 
+var  randomString = "New Zealand"
+
+console.log(randomString.length);
+
+console.log(randomString.substring(0,1))
+console.log(randomString.substring(0,2))
+console.log(randomString.substring(0,3))
+console.log(randomString.substring(0,4))
+console.log(randomString.substring(0,5))
+console.log(randomString.substring(0,6))
+console.log(randomString.substring(0,7))
+console.log(randomString.substring(0,8))
+console.log(randomString.substring(0,9))
+console.log(randomString.substring(0,10))
+console.log(randomString.substring(0,11))
+
